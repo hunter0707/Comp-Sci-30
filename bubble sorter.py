b1186881850c3f38f0d5b57dbce08b35d7590f50
Hunter Chen <34012681+hunter0707@users.noreply.github.com>
2019-09-11 13:44:49 -0600

    Update bubble sorter.py

--- bubble sorter.py
+++ bubble sorter.py
@@ -1,9 +1,26 @@
 n = input("enter numbers separated by space: ").split()
 
-def bubbleSort(n):
-    for i in n:
-        while 
-    
- 
-while bubbleSort(n) != n:
-    bubbleSort(n)
+def bubbleSort(n, b):
+        print("run")
+        print(n)
+        a = b
+        while a < (len(n) - 1):
+                if int(n[a]) > int(n[a+1]):
+                        n[a], n[a+1] = n[a+1], n[a]
+                        a += 1
+                        print(n)
+                else:   
+                        print(n)
+                        return n
+        return n
+
+a = 0
+
+def check(n):
+       
+
+while check(n) == False:
+        bubbleSort(n,a)
+        a += 1
+        if a == len(n):
+                a = 0
