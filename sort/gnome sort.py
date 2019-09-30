edf7619e8188e37754ba819ccd02747b356a1844
Hunter Chen <34012681+hunter0707@users.noreply.github.com>
2019-09-30 13:45:48 -0600

    Update gnome sort.py

--- sort/gnome sort.py
+++ sort/gnome sort.py
@@ -1,17 +1,21 @@
-n = '4 -1 25 13 19 -5 0 21 19 1'.split()
+n = '10 9 8 7 6 5 4 3 2 1'.split()
 n = [int(x) for x in n]
 
 def gnomeSort(n):
     r = 0
-    for i in range(len(n)):
-        print(i)
-        for a in range(i,-1,-1):
-            print(a,'a')
-            r += 1
-            if n[a] > n[a-1]:
-                n[a], n[a-1] = n[a-1], n[a]
-                r += 1
-    print(n)
+    a = 0
+    unsorted = True
+    while unsorted:
+        r += 1
+        #print(n, a, r)
+        if a > 0 and a < len(n) and n[a] < n[a-1]:
+            n[a], n[a-1] = n[a-1], n[a]
+            if a > 0:
+                a -= 1
+        else:
+            a += 1
+        if a == len(n):
+            unsorted = False
     return r
 
 print(gnomeSort(n))
