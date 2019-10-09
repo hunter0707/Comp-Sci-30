dfd9a07c37a3c133c7a730fcbfb553e27a906263
Hunter Chen <34012681+hydraxiler32@users.noreply.github.com>
2019-10-08 23:46:29 -0600

    Update bubble sorter.py

--- sort/bubble sorter.py
+++ sort/bubble sorter.py
@@ -1,19 +1,13 @@
 import random
-import time
 
 n = input("enter numbers separated by space: ").split()
-for i in range(0,1500,1):
-        n.append(i)
-random.shuffle(n)
 print(n)
-
-start_time = time.time()
 
 def bubbleSort(n):
         print(n)
         unsorted = True
         while unsorted:
-                #print(n, 'run')
+                print(n, 'run')
                 unsorted = False
                 for a in range(len(n) - 1):
                         if int(n[a]) > int(n[a+1]): #if current greater then next
@@ -22,4 +16,3 @@ def bubbleSort(n):
         return n
    
 print(bubbleSort(n), 'finished')
-print(time.time() - start_time)
